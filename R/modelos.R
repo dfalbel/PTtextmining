f94374a21d4178c7748f8a2c488f7214d23fa88e
Daniel Falbel <dfalbel@gmail.com>
2016-03-16 13:42:19 -0400

    função p/ ngram

--- R/modelos.R
+++ R/modelos.R
@@ -1,13 +1,15 @@
 #' Matriz de termos e documentos
 #' 
 #' @param s vetor de chr
+#' @param n n usado no n-gram. Esse argumento é usado se você quiser ao invés de
+#' uma matriz de termos, uma matriz com os n-grams.
 #' @param tipo tipo de matriz (contagem ou flag)
 #' @return uma matriz de termos e documentos
 #' 
 #' @export
-mtd <- function(s, tipo = "contagem"){
+mtd <- function(s, n = 1, tipo = "contagem"){
   
-  p <- stringr::str_split(s, "[:space:]")
+  p <- ngram(s, n = n)
   names(p) <- 1:length(p)
   
   m <- plyr::ldply(p, function(c){
@@ -26,4 +28,26 @@ mtd <- function(s, tipo = "contagem"){
   }
   
   return(m)
+}
+
+#' NGRAM
+#' 
+#' @param s vetor de chr
+#' @param n n do n-gram (quantas palavras juntas)
+#'
+#' @return uma lista com os n-grams para cada elemento do vetor.
+#' @seealso \code{\link{mtd}}
+#'
+#' @export
+ngram <- function(s, n = 1){
+  n <- n - 1
+  p <- stringr::str_split(s, "[:space:]")
+  if(n > 0){
+    p <- plyr::llply(p, function(c){
+      plyr::laply(1:(length(c) - n), function(i, c){
+        paste(c[i + (0:n)], collapse = " ")
+      }, c = c)
+    })
+  }
+  return(p)
 }
