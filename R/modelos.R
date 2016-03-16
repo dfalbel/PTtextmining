a968864e79d579e55c123f674884f04c70323c64
Daniel Falbel <dfalbel@gmail.com>
2016-03-16 14:32:15 -0400

    palavras seguidas daquela palavra

--- R/modelos.R
+++ R/modelos.R
@@ -51,3 +51,60 @@ ngram <- function(s, n = 1){
   }
   return(p)
 }
+
+#' Detectar palavra
+#' 
+#' @param s vetor de chr
+#' @param p palavra que deseja detectar
+#'
+#' @export
+detectar <- function(s, p){
+  stringr::str_detect(s, paste0("\\b", p, "\\b", collapse = ""))
+}
+
+#' Palavras seguidas
+#' 
+#' Quais são as palavras que mais aparecem antes ou depois da palavra p.
+#' @param s vetor de chr
+#' @param p palavra que deseja buscar
+#' @param qtd quantidade de palavras que você quer mostrar (se = NULL, mostrar 
+#' todos)
+#'
+#' @return um data.frame com as colunas ng_a palavra que mais veio antes e n_a 
+#' a quantidade de vezes (qtd). ng_d são as palavras que aparecem depois do termo p.
+#'
+#' @export
+palavras_seguidas <- function(s, p, qtd = NULL){
+  
+  ng <- ngram(s, n = 2) %>% unlist
+  ng <- ng[detectar(ng, p)]
+  
+  ng_depois <- ng[stringr::str_detect(ng, paste0("\\b", p, " ", collapse = ""))]
+  ng_antes <- ng[stringr::str_detect(ng, paste0(" ", p, "\\b", collapse = ""))]
+  
+  depois <- dplyr::data_frame(ng_d = ng_depois) %>%
+    dplyr::group_by(ng_d) %>%
+    dplyr::summarise(n_d = n()) %>%
+    dplyr::arrange(desc(n_d)) %>%
+    dplyr::mutate(ng_d = ng_d %>% remover_palavra(p) %>% 
+                    remover_espacos_excedentes()
+                  )
+  
+  antes <- dplyr::data_frame(ng_a = ng_antes) %>%
+    dplyr::group_by(ng_a) %>%
+    dplyr::summarise(n_a = n()) %>%
+    dplyr::arrange(desc(n_a)) %>%
+    dplyr::mutate(ng_a = ng_a %>% remover_palavra(p) %>% 
+                    remover_espacos_excedentes()
+                  )
+  
+  r <- dplyr::bind_cols(
+    antes, depois
+  )
+  if(!is.null(qtd)){
+    dplyr::slice(1:qtd)
+  }
+  return(r)
+}
+
+
